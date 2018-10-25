2728e85f0a4c21ad72862b20095939869a0944ea
ddiez31 <david.diez31@free.fr>
2018-10-25 12:02:51 +0200

    clean files

--- post.ts
+++
@@ -1,8 +0,0 @@
-export class Post {
-    public created_at: number;
-    public loveIts: number;
-    constructor(public title: string, public content: string) {
-        this.created_at = new Date().getTime();
-        this.loveIts = 0;
-    }
-}
